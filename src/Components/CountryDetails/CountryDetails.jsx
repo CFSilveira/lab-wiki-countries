219367f38b4fe125484beaaa2f1cb8a01b574f10
Carlos Silveira <carloss.87@gmail.com>
2022-03-03 23:04:05 +0000

    further changes

---
+++ src/Components/CountryDetails/CountryDetails.jsx
@@ -0,0 +1,35 @@
+import React, {useState, useEffect} from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+
+function CountryDetails() {
+    let { id } = useParams();
+    const [rightone, setRightone] = useState(null)
+
+        useEffect(() => {
+            axios.get(`https://ih-countries-api.herokuapp.com/countries/${id}`)
+              .then((response) => {
+                console.log(response.data);
+                setRightone(response.data);
+              })
+              .catch((err) => {
+                console.log(err);
+              });
+          }, [id]);
+    
+    
+
+  return (
+    <div>
+        {rightone && (
+        <>
+        <h1>{rightone.name.official}</h1>
+        <h3> Catipal City: {rightone.capital}</h3>
+        <p>Official Languages: {rightone.languages.prs}</p>
+        </>          
+        )}
+    </div>
+  )
+}
+
+export default CountryDetails
